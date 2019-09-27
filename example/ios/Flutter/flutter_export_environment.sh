38707d5d5aabf7e1c8929a40365aab62b1b33b3c
Ricardo Pineda <pinedasoft@gmail.com>
2019-09-27 11:01:23 -0600

    Flutter SDK enviroment

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/rap/Documents/Projects/2018/flutter/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rap/Documents/Projects/2019/flutter-modular-apps/flutter-gramola-timeline/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rap/Documents/Projects/2018/flutter/development/flutter/bin/cache/artifacts/engine/ios"
